8240a7771c34b18966ef15e358486d8802133f66
mistermaxis <55718118+mistermaxis@users.noreply.github.com>
2020-01-01 16:02:16 -0300

    Add project files.

---
+++ WpfTestingXAML/FirstPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfTestingXAML
+{
+    /// <summary>
+    /// Interaction logic for FirstPage.xaml
+    /// </summary>
+    public partial class FirstPage : Page
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Yeah");
+        }
+
+    }
+}
---
+++ WpfTestingXAML/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace WpfTestingXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        { 
+            MessageBox.Show("Something");
+        }
+    }
+}
